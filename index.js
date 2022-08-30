67ae2950ba8eb61322b723e231746ce8f59abc54
Mohammed_Thasleem <mohammedthasleempm@gmail.com>
2022-08-30 18:51:33 +0530

    project completed

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
     const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    const root = document.documentElement;
+    root.style.setProperty(`--${this.name}`, this.value + suffix);
   }
 
   inputs.forEach(input => input.addEventListener('change', handleUpdate));
